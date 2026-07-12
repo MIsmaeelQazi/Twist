dd11464aca896b821e6f5a3dc26cb0919beacaf2
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-07-12 12:22:36 +0000

    updated script

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ const Canvas = document.getElementById("GameScreen");
 const PaintBrush = Canvas.getContext("2d");
 
 function RoadAndMobs(){
+    // Road 
+    PaintBrush.fillStyle = "gray";
+    PaintBrush.fillRect(0, 0, 800, 800);
     
+    // Road Lines
+    PaintBrush.strokeStyle = "white";
+    PaintBrush.lineWidth = 3;
+    PaintBrush.setLineDash([25, 20]);
+
+    PaintBrush.beginPath();
+
+    PaintBrush.moveTo(397, 0);      // Middle of the road
+    PaintBrush.lineTo(397, Canvas.height);
+
+    PaintBrush.stroke();
+
+    PaintBrush.setLineDash([]);
+
+
+
 }
 
 
 
-function CarLogic(){
+function Car(){
     // Draw Main Car 
     PaintBrush.fillStyle = "blue";
     PaintBrush.fillRect(100, 100, 50, 50);
