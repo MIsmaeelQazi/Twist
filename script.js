7b99d6391982bab772aff6dcfda121d354374737
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-07-12 09:14:20 +0000

    Minor fixes

---
+++ script.js
@@ -0,0 +1,17 @@
+const Canvas = document.getElementById("GameScreen");
+const PaintBrush = Canvas.getContext("2d");
+
+function RoadAndMobs(){
+    
+}
+
+
+
+function CarLogic(){
+    // Draw Main Car 
+    PaintBrush.fillStyle = "blue";
+    PaintBrush.fillRect(100, 100, 50, 50);
+
+
+
+}
